59995f92e2906b48f49c7744773d27da31cef3a7
fajarbc <bcfajar@gmail.com>
2021-07-27 10:24:57 +0700

    Fix & refactor datatable

--- app/Controllers/MasterPromoCodes.php
+++ app/Controllers/MasterPromoCodes.php
@@ -14,7 +14,7 @@ class MasterPromoCodes extends BaseController
 		$this->admin_model = new AdminsModel();
 		$this->db = \Config\Database::connect();
 		$this->table_name = 'master_promo_codes';
-		$this->builder = $this->db->table($this->table_name);
+		$this->builder = $this->db->table("$this->table_name as t");
 	}
 
 	public function index()
@@ -40,6 +40,34 @@ class MasterPromoCodes extends BaseController
 		ini_set('memory_limit', '-1');
 		$req = $this->request;
 
+		// fields order 0, 1, 2, ...
+		$fields_order = array(
+			null,
+			"t.id",
+			"t.code",
+			"t.status",
+			"t.updated_at",
+		);
+		// fields to search with
+		$fields_search = array(
+			"t.code",
+		);
+		// select fields
+		$select_fields = 't.id,t.promo_id,t.code,t.status,t.updated_at,t.updated_by';
+
+		// building where query
+		$status = isset($_REQUEST['status']) ? (int)$req->getVar('status') : '';
+		$where = array('t.deleted_at' => null);
+		if ($status == 1) $where += array('t.status' => 1);
+		elseif ($status == 2) $where += array('t.status' => 0);
+
+		// add select and where query to builder
+		$this->builder
+			->select($select_fields)
+			->where($where);
+
+
+		// bulding order query
 		$order = $req->getVar('order');
 		$length = isset($_REQUEST['length']) ? (int)$req->getVar('length') : 10;
 		$start = isset($_REQUEST['start']) ? (int)$req->getVar('start') : 0;
@@ -50,66 +78,27 @@ class MasterPromoCodes extends BaseController
 			$dir = $order[0]['dir'];
 		}
 		if ($dir != "asc" && $dir != "desc") $dir = "asc";
+		if (isset($fields_order[$col])) $this->builder->orderBy($fields_order[$col],  $dir); // add order query to builder
 
-		$columns_order = array(
-			null,
-			"t1.id",
-			"t1.code",
-			"t1.status",
-			"t1.updated_at",
-		);
-		$columns_search = array(
-			"t1.code",
-		);
-		$data_rows = array();
-		$data_rows_filtered = array();
-		$status = isset($_REQUEST['status']) ? (int)$req->getVar('status') : '';
-		$where = array('t1.deleted_at' => null);
-		if ($status == 1) $where += array('t1.status' => 1);
-		elseif ($status == 2) $where += array('t1.status' => 0);
-
-		$this->builder
-		// ->start_cache()
-			->from("$this->table_name as t1")
-			->select('t1.id,t1.promo_id,t1.code,t1.status,t1.updated_at,t1.updated_by')
-			->where($where);
-		if (isset($columns_order[$col])) $this->builder->orderBy($columns_order[$col],  $dir);
-		if (isset($columns_order[$col])) $this->builder->orderBy($columns_order[$col],  $dir);
+		// bulding search query
 		if (!empty($req->getVar('search')['value'])) {
 			$search = $req->getVar('search')['value'];
 			$search_array = array();
-			foreach ($columns_search as $key) $search_array[$key] = $search;
-			$this->builder->groupStart()
+			foreach ($fields_search as $key) $search_array[$key] = $search;
+			// add search query to builder
+			$this->builder
+				->groupStart()
 				->orLike($search_array)
 				->groupEnd();
 		}
-		// $this->builder->stop_cache();
-		$data_rows = $this->builder->get()->getResultArray();
-		// dd($data_rows);
-		$this->builder
-		// ->start_cache()
-			->from("$this->table_name as t1")
-			->select('t1.id,t1.promo_id,t1.code,t1.status,t1.updated_at,t1.updated_by')
-			->where($where);
-		if (isset($columns_order[$col])) $this->builder->orderBy($columns_order[$col],  $dir);
-		if (isset($columns_order[$col])) $this->builder->orderBy($columns_order[$col],  $dir);
-		// dd($req->getVar('search'));
-		if (!empty($req->getVar('search')['value'])) {
-			$search = $req->getVar('search')['value'];
-			$search_array = array();
-			foreach ($columns_search as $key) $search_array[$key] = $search;
-			$this->builder->groupStart()
-				->orLike($search_array)
-				->groupEnd();
-		}
-		$this->builder->limit($length, $start);
-		$data_rows_filtered = $this->builder->get()->getResultArray();
-		// $this->builder->flush_cache();
-		$totalFiltered = count($data_rows);
-		// echo $this->db->last_query();die;
+		$totalData = count($this->builder->get(0, 0, false)->getResult()); // 3rd parameter is false to NOT reset query
+
+		$this->builder->limit($length, $start); // add limit for pagination
+		$dataResult = array();
+		$dataResult = $this->builder->get()->getResult();
 
 		$data = array();
-		if (!empty($data_rows_filtered)) {
+		if (count($dataResult) > 0) {
 			$i = $start;
 			$btn_disabled = ' disabled';
 			$btn_hide = ' d-none';
@@ -117,38 +106,38 @@ class MasterPromoCodes extends BaseController
 			// 	$btn_disabled = '';
 				$btn_hide = '';
 			// }
-			foreach ($data_rows_filtered as $row) {
+			// looping through data result
+			foreach ($dataResult as $row) {
 				$i++;
 
-				$btn_edit_data = 'data-id="' . $row['id'] . '"
-				data-code="' . $row['code'] . '"
-				data-status="' . $row['status'] . '"
+				$btn_edit_data = 'data-id="' . $row->id . '"
+				data-code="' . $row->code . '"
+				data-status="' . $row->status . '"
 				';
 				$action = '
 				<button class="btn btn-xs mb-2 btn-success btnAction btnEdit '.$btn_hide.'" title="Edit Kode Promo" ' . $btn_edit_data . ' ' . $btn_disabled . '><i class="fa fa-edit"></i> Edit</button>
-				<br><button class="btn btn-xs mb-2 btn-danger btnAction btnDelete '.$btn_hide.'" title="Delete Kode Promo" data-id="' . $row['id'] . '" data-code="' . $row['code'] . '" ' . $btn_disabled . '><i class="fa fa-trash-o"></i> Delete</button>
+				<br><button class="btn btn-xs mb-2 btn-danger btnAction btnDelete '.$btn_hide.'" title="Delete Kode Promo" data-id="' . $row->id . '" data-code="' . $row->code . '" ' . $btn_disabled . '><i class="fa fa-trash-o"></i> Delete</button>
 				';
 
 				$r = array();
 				$r[] = $i;
-				$r[] = $row['id'];
-				$r[] = $row['code'];
-				$r[] = $row['status'] == 1 ? 'Active' : 'Inactive';
-				$r[] = $row['updated_at'].'<br>'.$row['updated_by'];
+				$r[] = $row->id;
+				$r[] = $row->code;
+				$r[] = $row->status == 1 ? 'Active' : 'Inactive';
+				$r[] = "$row->updated_at<br>$row->updated_by";
 				$r[] = $action;
 				$data[] = $r;
 			}
 		}
-		//$totalFiltered=count($data);
 
 		$json_data = array(
 			"draw"            => intval($req->getVar('draw')),   // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw. 
-			"recordsTotal"    => intval($totalFiltered),  // total number of records
-			"recordsFiltered" => intval($totalFiltered), // total number of records after searching, if there is no searching then totalFiltered = totalData
+			"recordsTotal"    => intval($totalData),  // total number of records
+			"recordsFiltered" => intval($totalData), // total number of records after searching, if there is no searching then totalFiltered = totalData
 			"data"            => $data   // total data array
 		);
 
-		echo json_encode($json_data);  // send data as json format
+		echo json_encode($json_data);
 	}
 
 	public function delete_kodepromo()
@@ -196,10 +185,10 @@ class MasterPromoCodes extends BaseController
 			$this->db->trans_begin();
 			if ($id > 0) {
 				// update code field in pameran
-				$code_data = $this->db->select('mt1.id,code,id_pameran')
+				$code_data = $this->db->select('mt.id,code,id_pameran')
 				->from('mapping_code mkp')
-				->join('code kp', 't1.id=mt1.id', 'left')
-				->where('mt1.id', $id)
+				->join('code kp', 't.id=mt.id', 'left')
+				->where('mt.id', $id)
 				->get()->row();
 				if($code_data) {
 					$pameran = $this->db->select('code')->where('id_pameran', $code_data->id_pameran)->get('pameran')->row();
