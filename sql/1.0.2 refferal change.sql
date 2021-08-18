48e1ab4b5dfdfceafca1f782e17b5cbd42c5adc3
Mohamad Ilham <mohamadilham25@gmail.com>
2021-08-18 14:14:04 +0700

    * sql from user id in user_balance

--- sql/1.0.2 refferal change.sql
+++ sql/1.0.2 refferal change.sql
@@ -1,3 +1,5 @@
 -- add column total transaction and saving
 
 ALTER TABLE `referrals` ADD `transaction` INT(8) NOT NULL DEFAULT '0' AFTER `status`, ADD `saving` VARCHAR(16) NOT NULL DEFAULT '0' AFTER `transaction`;
+
+ALTER TABLE `user_balance` ADD `from_user_id` INT(24) NULL AFTER `check_id`;
