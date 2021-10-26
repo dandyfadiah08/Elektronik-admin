7a8f22ca4db40a3ed8c9e9c58e44075bd27550ad
fajarbc <bcfajar@gmail.com>
2021-10-26 17:26:49 +0700

    resolve conflict from server

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace Config;
@@ -66,72 +65,3 @@ class Filters extends BaseConfig
 		]
 	];
 }
-=======
-<?php
-
-namespace Config;
-
-use CodeIgniter\Config\BaseConfig;
-use CodeIgniter\Filters\CSRF;
-use CodeIgniter\Filters\DebugToolbar;
-use CodeIgniter\Filters\Honeypot;
-
-class Filters extends BaseConfig
-{
-	/**
-	 * Configures aliases for Filter classes to
-	 * make reading things nicer and simpler.
-	 *
-	 * @var array
-	 */
-	public $aliases = [
-		'csrf'     => CSRF::class,
-		'toolbar'  => DebugToolbar::class,
-		'honeypot' => Honeypot::class,
-		'auth_api' => \App\Filters\AuthAPI::class,
-	];
-
-	/**
-	 * List of filter aliases that are always
-	 * applied before and after every request.
-	 *
-	 * @var array
-	 */
-	public $globals = [
-		'before' => [
-			// 'honeypot',
-			// 'csrf',
-		],
-		'after'  => [
-			'toolbar',
-			// 'honeypot',
-		],
-	];
-
-	/**
-	 * List of filter aliases that works on a
-	 * particular HTTP method (GET, POST, etc.).
-	 *
-	 * Example:
-	 * 'post' => ['csrf', 'throttle']
-	 *
-	 * @var array
-	 */
-	public $methods = [];
-
-	/**
-	 * List of filter aliases that should run on any
-	 * before or after URI patterns.
-	 *
-	 * Example:
-	 * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
-	 *
-	 * @var array
-	 */
-	public $filters = [
-		'auth_api' => [
-			'before' => ['/api/users/', '/api/users/*', '/api/device_check/', '/api/device_check/*']
-		]
-	];
-}
->>>>>>> 4ceb680f190ba5888faff33d0231bebcaea1154d
