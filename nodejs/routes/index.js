557fdd06635a06538766aa841f785b4ecb1d84fb
fajarbc <bcfajar@gmail.com>
2021-11-04 13:55:42 +0700

    Update nodejs bekas conflict dari git server

--- nodejs/routes/index.js
+++ nodejs/routes/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 
 const wrapper = () => {
@@ -15,21 +14,3 @@ const wrapper = () => {
   return router;
 };
 module.exports = wrapper;
-=======
-const express = require("express");
-
-const wrapper = () => {
-  var router = express.Router();
-  router.get("/", (req, res, next) => {
-    var env = req.app.get('env');
-    const url = `${env.HTTP}://${env.DOMAIN}`;
-    res.send(`<script>
-        alert('Redirecting to ${url}')
-        window.location = '${url}'
-        </script> `);
-  });
-
-  return router;
-};
-module.exports = wrapper;
->>>>>>> 4ceb680f190ba5888faff33d0231bebcaea1154d
